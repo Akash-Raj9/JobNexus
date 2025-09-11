3b94a73b8c564c5ad4091a105afcbd1055cb10d0
Akash-Raj9 <gopakash74@gmail.com>
2025-09-11 19:16:42 +0530

    api in env

--- Frontend/src/utils/data.js
+++ Frontend/src/utils/data.js
@@ -1,4 +1,4 @@
-export const USER_API_ENDPOINT = "http://localhost:5011/api/user";
-export const JOB_API_ENDPOINT = "http://localhost:5011/api/job";
-export const APPLICATION_API_ENDPOINT = "http://localhost:5011/api/application";
-export const COMPANY_API_ENDPOINT = "http://localhost:5011/api/company";
+export const USER_API_ENDPOINT = `${import.meta.env.VITE_USER_API_ENDPOINT}/user`;
+export const JOB_API_ENDPOINT = `${import.meta.env.VITE_USER_API_ENDPOINT}/job`;
+export const APPLICATION_API_ENDPOINT = `${import.meta.env.VITE_USER_API_ENDPOINT}/application`;
+export const COMPANY_API_ENDPOINT = `${import.meta.env.VITE_USER_API_ENDPOINT}/company`;
